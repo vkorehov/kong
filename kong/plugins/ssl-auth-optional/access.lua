9c6135b9bff55413bc51e5db84c6a9661870e181
vladlv <vladislavs.korehovs@ideaportriga.lv>
2016-07-21 16:31:33 +0300

    aaa

--- kong/plugins/ssl-auth-optional/access.lua
+++ kong/plugins/ssl-auth-optional/access.lua
@@ -2,7 +2,9 @@ local _M = {}
 
 function _M.execute(conf)
   if ngx.var.ssl_client_verify == "SUCCESS" then
+    ngx.log(ngx.ERR, "SSL S-DN: "..ngx.var.ssl_client_s_dn)
     local user = string.match(ngx.var.ssl_client_s_dn, '/CN=([^/]+)/')
+    ngx.log(ngx.ERR, "SSL USER: "..user)
     ngx.ctx.ssl_authenticated = true
     ngx.ctx.ssl_authenticated_credential = {username=user}
   else
