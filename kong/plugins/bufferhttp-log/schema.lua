fc2088c2cbbed338af26b442531b7eba1c3f90b6
Arturs <asirius4@inbox.lv>
2016-09-28 13:38:04 +0300

    Added new plugin: bufferhttp.log

---
+++ kong/plugins/bufferhttp-log/schema.lua
@@ -0,0 +1,14 @@
+return {
+  fields = {
+    retry_count = {type = "number", default = 10},
+    queue_size = {type = "number", default = 1000},
+    queue_size_mb = {type = "number", default = 20},
+    max_msg_size_mb = {type = "number", default = 2},
+    max_sending_queue_size_mb = {type = "number", default = 200},
+    flush_timeout = {type = "number", default = 2},
+    log_bodies = {type = "boolean", default = false},
+    connection_timeout = {type = "number", default = 30},
+    endpoint = {type = "string", required = true, default = "http://"},
+    https_verify = {type = "boolean", default = false}
+  }
+}
