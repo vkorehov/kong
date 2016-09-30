50befd9a653e8fd38ff1367bfea2aaaa2019c1c6
Arturs <asirius4@inbox.lv>
2016-09-30 16:54:41 +0300

    Update alf.lua

--- kong/plugins/bufferhttp-log/alf.lua
+++ kong/plugins/bufferhttp-log/alf.lua
@@ -40,12 +40,10 @@ local _mt = {
   __index = _M
 }
 
-function _M.new(log_bodies,max_msg_size,secure_message,secure_patterns)
+function _M.new(log_bodies,max_msg_size)
   local alf = {
     log_bodies = log_bodies,
     max_msg_size = max_msg_size,
-    secure_message = secure_message,
-    secure_patterns = secure_patterns,
     entries = {}
   }
 
