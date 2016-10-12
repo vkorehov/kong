92b2e6b3c8b9882261d0fc40e26c1616d8068b45
Arturs <asirius4@inbox.lv>
2016-10-12 17:43:38 +0300

    Update handler.lua

--- kong/plugins/mocker/handler.lua
+++ kong/plugins/mocker/handler.lua
@@ -3,6 +3,7 @@ local responses = require "kong.tools.responses"
 
 local cjson = require "cjson"
 local meta = require "kong.meta"
+local req_get_uri_args = ngx.req.get_uri_args
 
 --local server_header = _KONG._NAME.."/".._KONG._VERSION
 local server_header = meta._NAME.."/"..meta._VERSION
@@ -13,9 +14,9 @@ local Mocker = BasePlugin:extend()
 --Set Priority
 Mocker.PRIORITY = 1
 
-local function send_response(status_code,content, contentTypeJson)
+local function send_response(status_code,content, contentTypeJson,transformMessage)
     ngx.status = status_code
-    if contentTypeJson then
+    if contentTypeJson == "application/json; charset=utf-8" then
      ngx.header["Content-Type"] = "application/json; charset=utf-8"
     else
     ngx.header["Content-Type"] = "text/html; charset=UTF-8"    
@@ -23,7 +24,7 @@ local function send_response(status_code,content, contentTypeJson)
     
     ngx.header["Server"] = server_header
   
-    if contentTypeJson then
+    if contentTypeJson == "application/json; charset=utf-8" and transformMessage then
         if type(content) == "table" then
           ngx.say(cjson.encode(content))
         elseif content then
@@ -45,21 +46,53 @@ function Mocker:access(conf)
   
   local errorCode = 403
   local errorMessage = "This service is not available right now"
-  local contentTypeJson = true
+  local contentType = "application/json; charset=utf-8"
+  local transformMessage = true
     
-  if conf.error_code and type(conf.error_code) == "number" then
-      errorCode = conf.error_code
-  end
+  if conf.use_query_params and type(conf.use_query_params) == "boolean" then
+    local querystring = req_get_uri_args()
+    local querystringValue = querystring["mock"]
+    local mockValue = {}
+    local queryValueMAP = {}   
+        
+    if querystringValue then
+        if conf.query_param_mapping == nil then
+            queryValueMAP = {['mock1']={['code']=404,['contentType']='application/json; charset=utf-8',['message']='{\"message\":\"Service is Not Available\"}'},['mock2']={['code']=403,['contentType']='text/html; charset=UTF-8',['message']='<html><h1>Service is Not Available</h1></html>'}}
+        else
+            queryValueMAP = loadstring("return "..conf.query_param_mapping)()
+        end
+        
+        mockValue = queryValueMAP[querystringValue]
+        if mockValue then
+          if mockValue["code"] then
+            errorCode = mockValue["code"]
+          end
+          if mockValue["message"] then
+            transformMessage = false
+            errorMessage = mockValue["message"]
+          end
+           if mockValue["contentType"] then
+            contentType = mockValue["contentType"]
+          end
+        end
+    end
+ 
+        
+  else
+      if conf.error_code and type(conf.error_code) == "number" then
+          errorCode = conf.error_code
+      end
 
-  if type(conf.content_type_json) == "boolean" then
-      contentTypeJson = conf.content_type_json
+      if type(conf.content_type_json) == "boolean" and conf.content_type_json == false then
+          contentType = "text/html; charset=UTF-8"
+      end
+
+      if conf.error_message and type(conf.error_message) == "string" then
+          errorMessage = conf.error_message
+      end
   end
     
-  if conf.error_message and type(conf.error_message) == "string" then
-      errorMessage = conf.error_message
-  end
-
-  send_response(errorCode, errorMessage,contentTypeJson)
+  send_response(errorCode, errorMessage,contentType,transformMessage)
 
 end
 
