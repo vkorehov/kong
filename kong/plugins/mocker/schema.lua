4b1cfc0c5ebf64eb70e231da5108b18a2a07ac77
Arturs <asirius4@inbox.lv>
2016-10-12 10:45:57 +0300

    Create schema.lua

---
+++ kong/plugins/mocker/schema.lua
@@ -0,0 +1,6 @@
+return {
+  fields = {
+    error_code = {type = "number", default = 403},
+    error_message = {type = "string", required = true, default = "This service is not available right now"},
+  }
+}
