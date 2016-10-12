9b1894a5758aa397e417417c77f11f73fab87265
Arturs <asirius4@inbox.lv>
2016-10-12 11:48:07 +0300

    Update handler.lua

--- kong/plugins/mocker/handler.lua
+++ kong/plugins/mocker/handler.lua
@@ -1,11 +1,40 @@
 local BasePlugin = require "kong.plugins.base_plugin"
 local responses = require "kong.tools.responses"
 
+local cjson = require "cjson"
+local meta = require "kong.meta"
+
+--local server_header = _KONG._NAME.."/".._KONG._VERSION
+local server_header = meta._NAME.."/"..meta._VERSION
+
 --Extend Base Plugin
 local Mocker = BasePlugin:extend()
 
 --Set Priority
 Mocker.PRIORITY = 1
+
+local function send_response(status_code,content, contentTypeJson)
+    ngx.status = status_code
+    if contentTypeJson then
+     ngx.header["Content-Type"] = "application/json; charset=utf-8"
+    else
+    ngx.header["Content-Type"] = "text/html; charset=UTF-8"    
+    end
+    
+    ngx.header["Server"] = server_header
+  
+    if contentTypeJson then
+        if type(content) == "table" then
+          ngx.say(cjson.encode(content))
+        elseif content then
+          ngx.say(cjson.encode {message = content})
+        end
+    else
+        ngx.say(content)
+    end
+
+    return ngx.exit(status_code)
+end
 
 function Mocker:new()
   Mocker.super.new(self, "mocker")
@@ -16,17 +45,21 @@ function Mocker:access(conf)
   
   local errorCode = 403
   local errorMessage = "This service is not available right now"
-  local headers = {}
-  
+  local contentTypeJson = true
+    
   if conf.error_code and type(conf.error_code) == "number" then
       errorCode = conf.error_code
   end
 
+  if type(conf.content_type_json) == "boolean" then
+      contentTypeJson = conf.content_type_json
+  end
+    
   if conf.error_message and type(conf.error_message) == "string" then
       errorMessage = conf.error_message
   end
 
-  responses.send(errorCode, errorMessage)
+  send_response(errorCode, errorMessage,contentTypeJson)
 
 end
 
