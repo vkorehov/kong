ac53d33c14800b4e11ab6c2893ac2c0337470f95
Arturs <asirius4@inbox.lv>
2016-11-10 16:22:15 +0200

    Update buffer.lua

--- kong/plugins/bufferhttp-log/buffer.lua
+++ kong/plugins/bufferhttp-log/buffer.lua
@@ -224,6 +224,8 @@ function _M.new(conf)
     return nil, "log_bodies must be a boolean"
   elseif conf.secure_message ~= nil and type (conf.secure_message) ~= "boolean" then
     return nil, "secure_message must be a boolean"
+  elseif type(conf.default_app_key) ~= "string" then
+    return nil, "default app key must be a string"	
   elseif conf.retry_count ~= nil and type(conf.retry_count) ~= "number" then
     return nil, "retry_count must be a number"
   elseif conf.connection_timeout ~= nil and type(conf.connection_timeout) ~= "number" then
@@ -251,7 +253,7 @@ function _M.new(conf)
     queue_size         	    	= conf.queue_size or 1000,
     queue_sizeMB        	= conf.queue_size_mb * 2^20 or 20 * 2^20,  
     max_sending_queue_size  	= conf.max_sending_queue_size_mb * 2^20 or 200 * 2^20,
-    cur_alf              	= alf_serializer.new(conf.log_bodies,conf.max_msg_size_mb,conf.secure_message,conf.secure_patterns),
+    cur_alf              	= alf_serializer.new(conf.log_bodies,conf.max_msg_size_mb,conf.secure_message,conf.secure_patterns,conf.default_app_key),
     sending_queue      	    	= {},                             -- FILO queue
     sending_queue_size 	    	= 0,
     last_t              	= huge,
